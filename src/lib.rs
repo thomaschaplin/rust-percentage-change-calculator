eed68f2bca0e4fae434d24b4a346ee1822cacec5
Thomas Chaplin <thomaschaplin@outlook.com>
2021-12-11 23:47:38 +0000

    reduce to one function

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 use std::cmp::Ordering;
 use std::env;
 
-const INCREASE: &str = "+";
-const DECREASE: &str = "-";
-const EQUAL: &str = "";
-
 pub fn get_input(index: usize) -> u32 {
     let args: Vec<String> = env::args().collect();
     let result: u32 = match args[index].trim().parse() {
@@ -17,25 +13,17 @@ pub fn get_input(index: usize) -> u32 {
     result
 }
 
-pub fn calculate_percentage_increase(first: u32, second: u32) -> f32 {
-    (second - first) as f32 / first as f32 * 100.0
-}
-
-pub fn calculate_percentage_decrease(first: u32, second: u32) -> f32 {
-    (first - second) as f32 / first as f32 * 100.0
-}
-
-pub fn match_increase_or_decrease(first: u32, second: u32) -> (String, f32) {
+pub fn calculate_percentage_change(first: u32, second: u32) -> (String, f32) {
     match first.cmp(&second) {
         Ordering::Less => (
-            INCREASE.to_string(),
-            calculate_percentage_increase(first, second),
+            "+".to_string(),
+            (second - first) as f32 / first as f32 * 100.0,
         ),
         Ordering::Greater => (
-            DECREASE.to_string(),
-            calculate_percentage_decrease(first, second),
+            "-".to_string(),
+            (first - second) as f32 / first as f32 * 100.0,
         ),
-        Ordering::Equal => (EQUAL.to_string(), 0.0),
+        Ordering::Equal => ("".to_string(), 0.0),
     }
 }
 
@@ -45,27 +33,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_calculate_percentage_increase() {
-        assert_eq!(calculate_percentage_increase(1, 2), 100.0);
-        assert_eq!(calculate_percentage_increase(1, 1), 0.0);
-    }
-
-    #[test]
-    fn test_calculate_percentage_decrease() {
-        assert_eq!(calculate_percentage_decrease(2, 1), 50.0);
-        assert_eq!(calculate_percentage_decrease(1, 1), 0.0);
-    }
-
-    #[test]
     fn test_match_increase_or_decrease() {
-        assert_eq!(
-            match_increase_or_decrease(2, 1),
-            (DECREASE.to_string(), 50.0)
-        );
-        assert_eq!(
-            match_increase_or_decrease(1, 2),
-            (INCREASE.to_string(), 100.0)
-        );
-        assert_eq!(match_increase_or_decrease(1, 1), (EQUAL.to_string(), 0.0));
+        assert_eq!(calculate_percentage_change(2, 1), ("-".to_string(), 50.0));
+        assert_eq!(calculate_percentage_change(1, 2), ("+".to_string(), 100.0));
+        assert_eq!(calculate_percentage_change(1, 1), ("".to_string(), 0.0));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod lib;
 use lib::get_input;
-use lib::match_increase_or_decrease;
+use lib::calculate_percentage_change;
 
 fn main() {
     let first = get_input(1);
     let second = get_input(2);
-    let (symbol, percentage_change) = match_increase_or_decrease(first, second);
+    let (symbol, percentage_change) = calculate_percentage_change(first, second);
     println!("{}{}%", symbol, percentage_change);
 }
