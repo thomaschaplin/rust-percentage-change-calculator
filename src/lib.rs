dd4745f06044d4f7b1c18cb0423ba4d5ddcd63a8
LordRatte <joshuastubbs6@gmail.com>
2021-12-12 00:13:03 +0200

    Add tests (#1)
    * Add tests
    
    * Update src/lib.rs
    
    * Update src/lib.rs
    
    * Update src/lib.rs
    
    * Update src/lib.rs
    
    Co-authored-by: Thomas Chaplin <thomaschaplin@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -38,3 +38,32 @@ pub fn match_increase_or_decrease(first: u32, second: u32) -> (String, f32) {
         Ordering::Equal => (EQUAL.to_string(), 0.0),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_calculate_percentage_increase() {
+        assert_eq!(calculate_percentage_increase(1, 2), 100.0);
+        assert_eq!(calculate_percentage_increase(1, 1), 0.0);
+    }
+
+    #[test]
+    fn test_calculate_percentage_decrease() {
+        assert_eq!(calculate_percentage_decrease(2, 1), 50.0);
+        assert_eq!(calculate_percentage_decrease(1, 1), 0.0);
+    }
+
+    #[test]
+    fn test_match_increase_or_decrease() {
+        assert_eq!(
+            match_increase_or_decrease(2, 1),
+            (DECREASE.to_string(), 50.0)
+        );
+        assert_eq!(
+            match_increase_or_decrease(1, 2),
+            (INCREASE.to_string(), 100.0)
+        );
+        assert_eq!(match_increase_or_decrease(1, 1), (EQUAL.to_string(), 0.0));
+    }
+}
